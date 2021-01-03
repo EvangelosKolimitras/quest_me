56b821602c0dcc3920210426d3ba524e657def30
Evangelos Kolimitras <evangeloskolimitras@outlook.com>
2021-01-03 22:13:20 +0100

    feature Users dispatcher added

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,13 @@ import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom'
 import { App } from './app'
 import { initStore } from './store'
+import { usersAction } from './actions';
+import { users } from './services';
 
+const { receiveUsers } = usersAction();
 
 const store = initStore()
+store.dispatch(receiveUsers(users));
 
 ReactDOM.render(
 	<Provider store={store}>
