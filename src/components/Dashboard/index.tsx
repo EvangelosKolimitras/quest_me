7bd3d770b408cb9bd4d57c13ce399fc55b8339a4
Evangelos Kolimitras <evangeloskolimitras@outlook.com>
2021-01-05 22:19:02 +0100

    feat: Renders the users in the dashboard component

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,30 @@
 import { Container } from '@material-ui/core'
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { IUsers } from '../../services/declarations'
+
+interface DefaultRootState {
+	users: IUsers
+}
 
 export const Dashboard: React.FC = () => {
+	let users = sortUsers(useSelector((state: DefaultRootState) => state.users))
 	return (
 		<Container maxWidth="md">
 			<h1>Dashboard</h1>
+			{
+				users.map(uid => <p key={uid}>{uid}</p>)
+			}
 		</Container>
 	)
 }
+
+const sortUsers = (users: IUsers) =>
+	Object
+		.keys(users)
+		.sort((a, b) =>
+			sort(users)(a, b)
+		)
+
+const calculate = (users: IUsers) => (x: string) => Object.keys(users[x].answers).length + users[x].questions.length
+const sort = (users: IUsers) => (a: string, b: string) => calculate(users)(b) - calculate(users)(a)
