95bd6ed4246886cc95fcad5f7edde847fe0ae47a
Evangelos Kolimitras <evangeloskolimitras@outlook.com>
2021-01-05 21:40:02 +0100

    chore: Removes unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import React from "react";
-import { useSelector } from "react-redux"
-import { IUsers } from "./services/declarations";
 import { Switch, Route } from 'react-router-dom'
 import { Dashboard } from "./components/Dashboard";
 import { NavigationBar } from "./components/NavigationBar";
 
-interface DefaultRootState {
-	users: IUsers
-}
-
 export const App: React.FC = React.memo(() => {
-	useSelector((state: DefaultRootState) => state.users)
 	return (
 		<div>
 			<NavigationBar />
