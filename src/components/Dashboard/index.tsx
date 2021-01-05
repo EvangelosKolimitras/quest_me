80752a0d34dd55a5e9ebaf6bad98bcce37e85db5
Evangelos Kolimitras <evangeloskolimitras@outlook.com>
2021-01-05 22:23:50 +0100

    refactor: Refactors the functions in dashboard com

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,30 +1,32 @@
 import { Container } from '@material-ui/core'
-import React from 'react'
 import { useSelector } from 'react-redux'
 import { IUsers } from '../../services/declarations'
 
-interface DefaultRootState {
-	users: IUsers
+interface DefaultRootState { users: IUsers }
+export function Dashboard() {
+	return render(sortUsers(useSelector((state: DefaultRootState) => state.users)))
 }
 
-export const Dashboard: React.FC = () => {
-	let users = sortUsers(useSelector((state: DefaultRootState) => state.users))
+function render(users: any[]) {
 	return (
 		<Container maxWidth="md">
 			<h1>Dashboard</h1>
-			{
-				users.map(uid => <p key={uid}>{uid}</p>)
-			}
+			{users.map(uid => <p key={uid}>{uid}</p>)}
 		</Container>
 	)
 }
 
-const sortUsers = (users: IUsers) =>
-	Object
+function sortUsers(users: IUsers) {
+	return Object
 		.keys(users)
-		.sort((a, b) =>
-			sort(users)(a, b)
+		.sort((a, b) => sort(users)(a, b)
 		)
+}
 
-const calculate = (users: IUsers) => (x: string) => Object.keys(users[x].answers).length + users[x].questions.length
-const sort = (users: IUsers) => (a: string, b: string) => calculate(users)(b) - calculate(users)(a)
+function calculate(users: IUsers) {
+	return (x: string) => Object.keys(users[x].answers).length + users[x].questions.length
+}
+
+function sort(users: IUsers) {
+	return (a: string, b: string) => calculate(users)(b) - calculate(users)(a)
+}
