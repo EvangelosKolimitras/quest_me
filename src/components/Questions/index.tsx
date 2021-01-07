c30fb47450c06b4f09ad805ab1c92682e45bf289
Evangelos Kolimitras <evangeloskolimitras@outlook.com>
2021-01-07 16:39:45 +0100

    feat: Adds top filtering btn without functionality

--- src/components/Questions/index.tsx
+++ src/components/Questions/index.tsx
@@ -1,4 +1,4 @@
-import { Container } from '@material-ui/core'
+import { Button, Container, Typography, makeStyles, Box } from '@material-ui/core'
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { IQuestions } from '../../services/declarations'
@@ -9,12 +9,42 @@ interface DefaultRootState {
 	authedUser: string
 }
 
-export const Questions: React.FC = () => render(useSelector((state: DefaultRootState) => check(state.authedUser)(state.questions)))
+export const Questions: React.FC = () => {
+	const useStyles = makeStyles({
+		header: {
+			margin: "2rem auto",
+			textAlign: "center"
+		},
+		heading: {
+			fontSize: "4rem",
+			textAlign: "center"
+		},
+		button: {
+			marginTop: "1.2rem",
+			marginLeft: ".5rem",
+			marginRight: ".5rem",
+		}
+	})
 
-const render = (questions: any) => {
+	const classes = useStyles();
+	return render(classes, useSelector((state: DefaultRootState) => check(state.authedUser)(state.questions)))
+}
+
+const render = (classes: { header: string, heading: string, button: string }, questions: any) => {
 	return (
 		<Container>
-			<h1>Questions</h1>
+			<Box component="div" className={classes.header}>
+				<Typography className={classes.heading} variant="h1">Questions</Typography>
+				<Button variant="outlined" color="primary" className={classes.button}>
+					All {questions(false).length + questions(false).length}
+				</Button>
+				<Button variant="contained" color="primary" className={classes.button}>
+					Not answered {questions(false).length}
+				</Button>
+				<Button variant="contained" color="secondary" className={classes.button}>
+					Answered {questions(true).length}
+				</Button>
+			</Box>
 			{ questions(true).map(((answer: any) => <QuestionItem key={answer.id} id={answer.id} />))}
 			{ questions(false).map(((answer: any) => <QuestionItem key={answer.id} id={answer.id} />))}
 		</Container>
