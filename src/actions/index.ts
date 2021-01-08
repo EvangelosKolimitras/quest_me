63484523540073c2d76b154ddc893af616446946
Evangelos Kolimitras <evangeloskolimitras@outlook.com>
2021-01-08 19:05:52 +0100

    feat: Implements dispatchers in initialization

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,22 +1,27 @@
 import { Dispatch } from 'redux';
-import { saveQuestion } from '../services';
-import { addQuestion } from './questions';
-import { addUserQuestions } from './users';
+import { initializations, saveQuestion } from '../services';
+import { addQuestion, receiveQuestions } from './questions';
+import { addUserQuestions, receiveUsers } from './users';
 
 export { usersAction } from './users';
 export { questionsAction } from './questions';
 export { authedUserAction } from './authedUser';
 
-export function addQuestionHandler(optionOne: string, optionTwo: string, author: string) {
-	return (dispatch: Dispatch) => {
-		saveQuestion({
+export const initiliazer = () =>
+	async (dispatch: Dispatch) => {
+		const questions: any = await (await initializations()).questions
+		const users: any = await (await initializations()).users
+		dispatch(receiveUsers(users));
+		dispatch(receiveQuestions(questions));
+	}
+
+export const addQuestionHandler = (optionOne: string, optionTwo: string, author: string) =>
+	async (dispatch: Dispatch) => {
+		const question: any = await saveQuestion({
 			optionOne,
 			optionTwo,
 			author
 		})
-			.then((question: any) => {
-				dispatch(addQuestion(question))
-				dispatch(addUserQuestions(question))
-			})
+		dispatch(addQuestion(question))
+		dispatch(addUserQuestions(question))
 	}
-}
