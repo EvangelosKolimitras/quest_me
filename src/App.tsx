68f570ebda3a54c6de92c5a5f08abee58defd053
Evangelos Kolimitras <evangeloskolimitras@outlook.com>
2021-01-09 17:56:11 +0100

    feat: Adds checks for initialization and authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { Switch, Route } from 'react-router-dom'
 import { Dashboard } from "./components/Dashboard";
@@ -8,24 +8,33 @@ import { NavigationBar } from "./components/NavigationBar";
 import { NewQuestion } from "./components/NewQuestion";
 import { Questions } from "./components/Questions";
 
-interface DefaultRootState { authedUser: string }
+interface DefaultRootState {
+	questions: any;
+	users: any;
+	authedUser: string
+}
 
 export const App: React.FC = React.memo(() => {
 	const authedUser = useSelector((state: DefaultRootState) => state.authedUser)
-	const [isAuthed, setIsAuthed] = useState(!authedUser)
-	return (
-		<>
-			{ifAuthedRenderNav(isAuthed)}
-			<Switch>
-				{ifAuthedRenderHome(isAuthed)}
-				{ifAuthedRenderQuestions(isAuthed)}
-				{ifAuthedRendeLeaders(isAuthed)}
-				{ifAuthedRenderNewQuestion(isAuthed)}
-				{ifNotAuthed(isAuthed)}
-			</Switch>
-		</>
-	)
+	const users = useSelector((state: DefaultRootState) => state.users)
+	const questions = useSelector((state: DefaultRootState) => state.questions)
+	const isAuthed = authedUser !== null
+	const isInitialized = [Object.keys(users), Object.keys(questions)].every((el: any[]) => el.length > 0)
+	const check = isInitialized && isAuthed
+	return jsx(check)
 })
+
+const jsx = (check: any) =>
+	<>
+		{ifAuthedRenderNav(check)}
+		<Switch>
+			{ifAuthedRenderHome(check)}
+			{ifAuthedRenderQuestions(check)}
+			{ifAuthedRendeLeaders(check)}
+			{ifAuthedRenderNewQuestion(check)}
+			{ifNotAuthed(check)}
+		</Switch>
+	</>
 
 const ifAuthedRenderNav = (authed: any) => authed && <NavigationBar />
 const ifAuthedRenderHome = (authed: any) => authed && <Route exact path="/" component={Home} />
