6218c463cb6cbd3db592201955b5d5c43530160d
unknown <evangeloskolimitras@outlook.com>
2021-01-23 00:00:01 +0100

    refactor: Adds object destructuring for env vars

--- server/src/connection/index.ts
+++ server/src/connection/index.ts
@@ -2,8 +2,9 @@ import { MongoClient } from "mongodb"
 import dotenv from "dotenv"
 
 dotenv.config()
+const {MONGO_DB_USERNAME:username,MONGO_DB_PASSWORD:password,MONGO_DB_DATABASE:database,MONGO_DB_DOMAIN:domain,MONGO_DB_OPTIONS:options} = process.env
 
-const client:MongoClient = new MongoClient(`mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_DOMAIN}/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,{useUnifiedTopology:true})
+const client:MongoClient = new MongoClient(`mongodb+srv://${username}:${password}@${domain}/${database}?${options}`,{useUnifiedTopology:true})
 export const connect = async () => {
     try {
         client.connect(e=>{
